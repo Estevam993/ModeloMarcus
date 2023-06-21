d1a00fbefe7bea221708ecd2ab42a970f0a9ab2a
Estevam993 <joaovestevamr@gmail.com>
2023-06-21 10:41:27 -0300

    section 4 and script

---
+++ script/buttonScript.js
@@ -0,0 +1,33 @@
+function arrowDown1(){
+    var au = document.querySelector(".au1")
+    var ad = document.querySelector(".ad1")
+    if(ad.style.display=="block"){
+        ad.style.display = 'none'
+        au.style.display = 'block'
+    }else{
+        au.style.display = 'none'
+        ad.style.display = 'block'
+    }
+}
+function arrowDown2(){
+    var au = document.querySelector(".au2")
+    var ad = document.querySelector(".ad2")
+    if(ad.style.display=="block"){
+        ad.style.display = 'none'
+        au.style.display = 'block'
+    }else{
+        au.style.display = 'none'
+        ad.style.display = 'block'
+    }
+}
+function arrowDown3(){
+    var au = document.querySelector(".au3")
+    var ad = document.querySelector(".ad3")
+    if(ad.style.display=="block"){
+        ad.style.display = 'none'
+        au.style.display = 'block'
+    }else{
+        au.style.display = 'none'
+        ad.style.display = 'block'
+    }
+}
