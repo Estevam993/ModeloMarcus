8a918b61f56f109c3e9e193e02d98d5c2bd688ea
Estevam993 <joaovestevamr@gmail.com>
2023-06-21 10:54:18 -0300

    button animation

--- script/buttonScript.js
+++ script/buttonScript.js
@@ -4,9 +4,20 @@ function arrowDown1(){
     if(ad.style.display=="block"){
         ad.style.display = 'none'
         au.style.display = 'block'
+
     }else{
         au.style.display = 'none'
         ad.style.display = 'block'
+    }
+    var textContainer = document.getElementById("textContainer");
+    var hiddenText = document.getElementById("hiddenText");
+
+    if (textContainer.style.maxHeight) {
+        textContainer.style.maxHeight = null;
+        hiddenText.style.opacity = 0;
+    } else {
+        textContainer.style.maxHeight = textContainer.scrollHeight + "px";
+        hiddenText.style.opacity = 1;
     }
 }
 function arrowDown2(){
@@ -19,6 +30,16 @@ function arrowDown2(){
         au.style.display = 'none'
         ad.style.display = 'block'
     }
+    var textContainer = document.getElementById("textContainer1");
+    var hiddenText = document.getElementById("hiddenText1");
+
+    if (textContainer.style.maxHeight) {
+        textContainer.style.maxHeight = null;
+        hiddenText.style.opacity = 0;
+    } else {
+        textContainer.style.maxHeight = textContainer.scrollHeight + "px";
+        hiddenText.style.opacity = 1;
+    }
 }
 function arrowDown3(){
     var au = document.querySelector(".au3")
@@ -29,5 +50,15 @@ function arrowDown3(){
     }else{
         au.style.display = 'none'
         ad.style.display = 'block'
+    }
+    var textContainer = document.getElementById("textContainer2");
+    var hiddenText = document.getElementById("hiddenText2");
+
+    if (textContainer.style.maxHeight) {
+        textContainer.style.maxHeight = null;
+        hiddenText.style.opacity = 0;
+    } else {
+        textContainer.style.maxHeight = textContainer.scrollHeight + "px";
+        hiddenText.style.opacity = 1;
     }
 }
